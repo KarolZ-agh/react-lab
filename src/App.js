0a9e5fab48da2e0edd913d9afca317f66eed3fa8
Student <student@d17-327e-pc-13.ipa.ii.agh.edu.pl>
2025-01-12 10:53:56 +0100

    added logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  var currentTitle = "The Matrix (1999)"
+  const [currentTitle, setTitle] = useState("The Matrix");
+  const [year, setYear] = useState("1999");
+
+  function onTitleChange(event) {
+    setTitle(event.target.value);
+    console.log(event.target.value);
+  }
 
 
-  function onInputChanged(event) {
-    console.log(event.target.value)
+  function onYearChange(event) {
+    let newValue = event.target.value;
+    if (newValue.length >= 4) {
+      setYear(event.target.value);
+      console.log(event.target.value);
+    }
   }
 
 
@@ -19,7 +30,7 @@ function App() {
     <div className="App">
       <div className="App-header">
          <h1>My favorite movies</h1>
-         <h2>Movie of a day: {currentTitle}</h2>
+         <h2>Movie of a day: {currentTitle} ({year})</h2>
       </div>
 
       <div class="column column-30">
@@ -31,15 +42,11 @@ function App() {
       <div class="column column-60 column-offset-5">
           <div class="single-input">
               <label>Title</label>
-              <input type="text" id="title-inpt" onChange={onInputChanged}/> 
+              <input type="text" id="title-inpt" value={currentTitle} onChange={onTitleChange}/> 
           </div>
           <div class="single-input">
               <label>Year</label>
-              <input type="text" id="year-inpt"/> 
-          </div>
-          <div class="single-input">
-              <label>Actors</label>
-              <input type="text" id="actors-inpt"/> 
+              <input type="text" id="year-inpt" value={year} onChange={onYearChange}/> 
           </div>
           <button id="add-btn" onClick={addMovie}>Add</button>
       </div>
